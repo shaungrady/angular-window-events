0023e5335434bbae9ea7adc02ebfb15b40d8b3d9
Shaun Grady <grady@shaungrady.com>
2017-02-03 19:07:33 -0800

    Stop double-testing on watch

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function (config) {
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    autoWatchBatchDelay: 1000,
+    autoWatchBatchDelay: 100,
     browsers: ['PhantomJS'],
     singleRun: true,
     concurrency: Infinity
